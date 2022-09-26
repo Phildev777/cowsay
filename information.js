3c9a57e6f06ad1230d91dd02f96cd9dcc46a61e8
Philippe Berra <102957058+Phildev777@users.noreply.github.com>
2022-09-26 17:10:04 +0200

    information change

--- information.js
+++ information.js
@@ -7,10 +7,3 @@ module.exports = {
     name: name,
     campus: campus,
 }
-var cowsay = require("cowsay");
-
-console.log(cowsay.say({
-    text: "I'm a moooodule",
-    e: "oO",
-    T: "U "
-}));
